5331154dda00afffd97232b4ebbfc7f330312dea
hafsafathy <hafsafathy020@gmail.com>
2023-12-01 14:50:15 -0800

    task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,25 +9,25 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+		return (tree_height(tree->left) - tree_height(tree->right));
 
 	return (0);
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * tree_height - Measures height of a binary tree.
  * @tree: pointer to the root.
  *
  * Return: If tree is NULL, your function must return 0.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
 		size_t eft = 0, rght = 0;
 
-		eft = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		rght = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		eft = tree->left ? 1 + tree_height(tree->left) : 1;
+		rght = tree->right ? 1 + tree_height(tree->right) : 1;
 		return ((eft > rght) ? eft : rght);
 	}
 	return (0);
